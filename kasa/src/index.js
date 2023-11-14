424db43bf429a320d4354050cbe325a16f868a83
vsabouroux <vsabouroux@aol.com>
2023-11-14 17:05:03 +0100

    début installation projet avec Router, fichiers .js fichier json

--- kasa/src/index.js
+++ kasa/src/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import "./index.css";
-import App from "./App";
+import App from "./views/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
