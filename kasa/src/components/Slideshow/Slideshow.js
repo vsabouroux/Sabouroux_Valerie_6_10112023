29023af2fec5736401faff4dd5c2c6148c700ddf
vsabouroux <vsabouroux@aol.com>
2023-11-28 18:50:06 +0100

    modification slideshow qd une seule image + correction scss

--- kasa/src/components/Slideshow/Slideshow.js
+++ kasa/src/components/Slideshow/Slideshow.js
@@ -1,32 +1,44 @@
 import React, { useState } from "react";
 import "./Slideshow.scss";
-
+//Ah zut ! j'aurais pu installer aussi le package  React Slider Responsive ! https://www.npmjs.com/package/react-responsive-carousel
 const Slideshow = ({ pictures }) => {
   const [currentSlide, setCurrentSlide] = useState(0);
 
+  // l'opérateur "%" permet de faire en sorte que lorsque l'UI arrive à la dernière photo s'il clique sur la suivante il reviendra à la 1ère
   const nextSlide = () => {
     setCurrentSlide((prevSlide) => (prevSlide + 1) % pictures.length);
   };
-
+  // ici la fonction fléchée va faire une sorte de boucle cad que si la diapo est la 1ere du tableau alors l'app va renvoyer la dernière photo
+  // et sinon ce sera la précédente dans le tableau
   const prevSlide = () => {
     setCurrentSlide((prevSlide) =>
       prevSlide === 0 ? pictures.length - 1 : prevSlide - 1
     );
   };
-
+  //
+  const hideButtons = pictures.length === 1;
   return (
     <div className="slideshow">
-      <button className="prev" onClick={prevSlide}>
-        &#8249;
-      </button>
-      <img src={pictures[currentSlide]} alt={`Slide ${currentSlide + 1}`} />
-      <button className="next" onClick={nextSlide}>
-        &#8250;
-      </button>
-
-      <div className="counter">
-        {currentSlide + 1} / {pictures.length}
-      </div>
+      {!hideButtons && (
+        <>
+          <button className="prev" onClick={prevSlide}>
+            &#8249;
+          </button>
+          <button className="next" onClick={nextSlide}>
+            &#8250;
+          </button>
+        </>
+      )}
+      <img
+        key={currentSlide}
+        src={pictures[currentSlide]}
+        alt={`Slide ${currentSlide + 1}`}
+      />
+      {!hideButtons && (
+        <div className="counter">
+          {currentSlide + 1} / {pictures.length}
+        </div>
+      )}
     </div>
   );
 };
