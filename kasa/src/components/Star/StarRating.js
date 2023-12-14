3e84f9f96b33f7b4fedf456ea155e86186701aaa
vsabouroux <vsabouroux@aol.com>
2023-12-14 14:42:53 +0100

    Tentative correction étoiles

--- kasa/src/components/Star/StarRating.js
+++ kasa/src/components/Star/StarRating.js
@@ -1,6 +1,8 @@
 //import PropTypes from "prop-types";
+import React from "react";
+import PropTypes from "prop-types";
 import Star from "./Star";
-
+import "./Star.scss";
 // ici avec Array.from on crée un tableau d'étoiles qui va être parcouru
 // et avec la condition "index<rating" cela veut dire que si la "lecture de la postion"
 //est inférieur au nombre d'étoiles du logement alors avec "map" on va colorer le bon nb d'étoiles
@@ -15,6 +17,9 @@ const StarRating = ({ rating }) => {
       ))}
     </div>
   );
+};
+StarRating.propTypes = {
+  rating: PropTypes.number.isRequired,
 };
 
 export default StarRating;
